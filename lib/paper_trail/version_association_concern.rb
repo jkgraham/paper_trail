e70ec1feacc212cbb1862f02972fda4945f988f3
Jonathan Graham <jkgraham@gmail.com>
2015-08-28 10:41:45 -0700

    Avoid 'uninitialized constant <SomePaydiciClass>::PaperTrail::Version' errors

--- lib/paper_trail/version_association_concern.rb
+++ lib/paper_trail/version_association_concern.rb
@@ -5,7 +5,7 @@ module PaperTrail
     extend ::ActiveSupport::Concern
 
     included do
-      belongs_to :version
+      belongs_to :version, class_name: 'PaperTrail::Version'
 
       attr_accessible :version_id, :foreign_key_name, :foreign_key_id if PaperTrail.active_record_protected_attributes?
     end
